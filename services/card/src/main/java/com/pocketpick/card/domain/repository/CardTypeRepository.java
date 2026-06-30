f62c4ddf416f602fe5644d9c87751c25992fb60f
허세진 <heosejin222@gmail.com>
2026-06-30 14:18:19 +0900

    fix: CardDetailResponse types를 CardTypeRepository 조회로 분리 (SCRUM-23)

---
+++ services/card/src/main/java/com/pocketpick/card/domain/repository/CardTypeRepository.java
@@ -0,0 +1,10 @@
+package com.pocketpick.card.domain.repository;
+
+import com.pocketpick.card.domain.domain.CardType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CardTypeRepository extends JpaRepository<CardType, Long> {
+    List<CardType> findByCardId(Long cardId);
+}
