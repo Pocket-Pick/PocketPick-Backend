b17846c1f66cb3b3784a3168c3e39c8eae786f7d
허세진 <heosejin222@gmail.com>
2026-07-02 11:29:03 +0900

    refactor: auth ErrorCode 인터페이스 및 AuthErrorCode Enum 도입 (SCRUM-35)

--- services/auth/src/main/java/com/pocketpick/auth/domain/domain/exception/DuplicateEmailException.java
+++ services/auth/src/main/java/com/pocketpick/auth/domain/domain/exception/DuplicateEmailException.java
@@ -1,9 +1,8 @@
 package com.pocketpick.auth.domain.domain.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class DuplicateEmailException extends AccountException {
+
     public DuplicateEmailException() {
-        super("DUPLICATE_EMAIL", "이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT);
+        super(AuthErrorCode.DUPLICATE_EMAIL);
     }
 }
