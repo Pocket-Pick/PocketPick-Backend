a87c827fdb513b16a8897fdb73b019a6cb639a05
허세진 <heosejin222@gmail.com>
2026-07-02 11:32:04 +0900

    refactor: user ErrorCode 인터페이스 및 UserErrorCode Enum 도입 (SCRUM-35)

--- services/user/src/main/java/com/pocketpick/user/domain/domain/exception/UserNotFoundException.java
+++ services/user/src/main/java/com/pocketpick/user/domain/domain/exception/UserNotFoundException.java
@@ -1,9 +1,8 @@
 package com.pocketpick.user.domain.domain.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class UserNotFoundException extends UserException {
+
     public UserNotFoundException() {
-        super("USER_NOT_FOUND", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+        super(UserErrorCode.USER_NOT_FOUND);
     }
 }
