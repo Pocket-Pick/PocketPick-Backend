375a7d5528b955d554b0890d7fe7045392692034
허세진 <heosejin222@gmail.com>
2026-07-10 23:04:05 +0900

    refactor: chat 서비스 ErrorCode 인터페이스 및 ChatErrorCode Enum 도입

--- services/chat/src/main/java/com/pocketpick/chat/global/exception/ChatRoomNotFoundException.java
+++ services/chat/src/main/java/com/pocketpick/chat/global/exception/ChatRoomNotFoundException.java
@@ -1,10 +1,10 @@
 package com.pocketpick.chat.global.exception;
 
-import org.springframework.http.HttpStatus;
+import com.pocketpick.chat.domain.exception.ChatErrorCode;
 
 public class ChatRoomNotFoundException extends BusinessException {
 
     public ChatRoomNotFoundException() {
-        super("CHAT_ROOM_NOT_FOUND", "채팅방을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+        super(ChatErrorCode.CHAT_ROOM_NOT_FOUND);
     }
 }
